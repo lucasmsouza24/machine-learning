fb8e0bfe85eab9a7b7b63234f58d390f4c31432d
lucasmsouza24 <lucas.msouza@bandtec.com.br>
2021-12-10 01:05:31 -0300

    convert to jupyter notebook

--- pandas/03.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-import matplotlib.pyplot as plt 
-
-df = pd.read_csv('/home/lucas/dev/machine-learning/data/covid_asia_weekly_trend.csv')
-
-print('size:', df.size)
-print('len:', len(df))
-print('shape:', df.shape)
-print('columns:', df.columns)
